e7d3bfa6029cf91c9745d2d5b93b4a84df973432
26th-Ara <101792772+Arafatsmart26@users.noreply.github.com>
2026-01-06 14:01:25 +0100

    Create Cpu.scala

---
+++ src/main/scala/Cpu.scala
@@ -0,0 +1,33 @@
+import chisel3._
+import chisel3.util._
+
+class Cpu extends Module {
+  val io = IO(new Bundle {
+    // senere: memory/bus/uart osv.
+  })
+
+  // -------------------------
+  // PC (program counter)
+  // -------------------------
+  val pc = RegInit(0.U(32.W))
+
+  // 32-bit instruction register (fetched instruction)
+  val instr = Wire(UInt(32.W))
+
+  // -------------------------
+  // FETCH stage (hardcoded)
+  // -------------------------
+  // addi x1, x0, 0x123  => 0x12300093
+  instr := "h12300093".U
+
+  // "Next PC" (for nu bare pc + 4)
+  pc := pc + 4.U
+}
+
+object CpuMain extends App {
+  println("Generating CPU Verilog...")
+  (new chisel3.stage.ChiselStage).emitVerilog(
+    new Cpu(),
+    Array("--target-dir", "generated")
+  )
+}
